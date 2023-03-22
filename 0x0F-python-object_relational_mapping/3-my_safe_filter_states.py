fec8a078e9f200bf12642a2fdc7c099514101816
dovoZuru <dovoZuru@aol.com>
2023-03-22 21:09:50 +0000

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]
+""" This module takes in an argument and displays all
+values in the states table in the database hbtn_0e_0_usa
+where name matches the argument and is safe from mysql
+injections.
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s\
+            ORDER BY states.id", (sys.argv[4], ))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
