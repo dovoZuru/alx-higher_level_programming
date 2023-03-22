eff2ee195c78229d764d12a724203db0d736a9cb
dovoZuru <dovoZuru@aol.com>
2023-03-22 21:02:20 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+""" This module takes in an argument and displays all
+values in ths states table in the database hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name =  '{}'\
+            ORDER BY states.id".format(sys.argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
